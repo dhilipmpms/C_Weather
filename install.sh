0c8b5f259e997631080acbb074ed1a9189d6ec1b
dhilipmpms <dhilipmpms16@gmail.com>
2026-01-10 23:38:48 +0530

    added if condition to check weather the raylib is aldready present then install

--- install.sh
+++ install.sh
@@ -1,19 +1,37 @@
-# to install the raylib module 
-git clone --depth=1 https://github.com/raysan5/raylib && 
-cd raylib/ &&
-mkdir build && 
-cd build && 
-cmake -DBUILD_SHARED_LIBS=ON .. &&
-make -j$(nproc) && 
-sudo make install &&
+#!/bin/bash
 
-#  To install curl.h module
-sudo apt install libcurl4-openssl-dev &&
+set -e
 
-#To install <cjson/cJSON.h>
-sudo apt install libcjson-dev && 
+echo "Starting installation..."
 
-#To install pkg-config
-sudo apt install pkg-config
+# -------------------------------
+# Install raylib (only if not present)
+# -------------------------------
+if [ ! -d "raylib" ]; then
+    echo "raylib not found. Installing raylib..."
 
+    git clone --depth=1 https://github.com/raysan5/raylib
+    cd raylib
+    mkdir build
+    cd build
+    cmake -DBUILD_SHARED_LIBS=ON ..
+    make -j$(nproc)
+    sudo make install
+
+    cd ../..
+else
+    echo "raylib already exists. Skipping."
+fi
+
+# -------------------------------
+# Install required system libraries
+# -------------------------------
+echo "Installing system dependencies..."
+sudo apt update
+sudo apt install -y \
+    libcurl4-openssl-dev \
+    libcjson-dev \
+    pkg-config
+
+echo "Installation completed successfully!"
 
